ede2552b18fcb940799097a97974f83884119f2a
Xavier <xavierx-men@hotmail.com>
2019-01-08 19:46:42 -0500

    Made some asio prototypes about communication

--- UnitWorld_server/Main.cpp
+++ UnitWorld_server/Main.cpp
@@ -1,3 +1,5 @@
+#include "lobby\ClientRoom.h"
+
 #include "communications/ServerConnector.h"
 
 #include "commons/Logger.hpp"
@@ -5,17 +7,18 @@
 #include <thread>
 #include <iostream>
 
-void clientCallback(std::shared_ptr<CommunicationHandler> communicationHandler)
-{
-	Logger::info("Client " + communicationHandler->prettyName() + " connected");
-}
-
 int main()
 {
 	Logger::registerInfo([](const std::string& message) {
 		std::cout << message;
 	});
+	ClientRoom clientRoom(2);
 	Logger::info("Waiting for connections...\n");
-	ServerConnector serverConnector(ConnectionInfo("127.0.0.1", "52124"), clientCallback);
+	ServerConnector serverConnector(
+		ConnectionInfo("127.0.0.1", "52124"),
+		[&clientRoom](std::shared_ptr<CommunicationHandler> communicationHandler){
+			clientRoom.addClient(communicationHandler);
+		}
+	);
 	serverConnector.accept();
 }
