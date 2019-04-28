6682b580f735fdf06841ebbb2b58acb31ca8376d
Xavier <xavierx-men@hotmail.com>
2019-04-28 14:29:40 -0400

    Made use of optional in mobile units

--- UnitWorld_shared/game/play/MobileUnit.h
+++ UnitWorld_shared/game/play/MobileUnit.h
@@ -2,12 +2,13 @@
 
 #include "Unit.h"
 
+#include "commons/Option.hpp"
+
 namespace uw
 {
     class MobileUnit : public Unit
     {
     public:
-        ~MobileUnit();
 
         virtual void actualize();
         void setDestination(const Vector2D& destination);
@@ -20,11 +21,10 @@ namespace uw
         const double stopDistanceFromTarget() const;
         const double stopDistanceFromTargetSq() const;
         Vector2D getBreakingAcceleration() const;
-        void deleteDestination();
         virtual const double maximumSpeed() const = 0;
         virtual const double maximumAcceleration() const = 0;
 
-        Vector2D* _destination;
+        Option<Vector2D> _destination;
         Vector2D _speed;
         Vector2D _acceleration;
     };
