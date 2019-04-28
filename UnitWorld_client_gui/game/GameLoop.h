c16a7bb2fb4845599cf42bce0a19cd4d5dbeeb67
abstrack <lebel.louisjacob@gmail.com>
2019-04-28 13:37:41 -0400

    clean GameLoop.*

--- UnitWorld_client_gui/game/GameLoop.h
+++ UnitWorld_client_gui/game/GameLoop.h
@@ -15,12 +15,8 @@ namespace uw
         void loop();
 
     private:
-        static const bool GAME_STATE_ENDED = true;
-        static const bool GAME_STATE_RUNNING = false;
-
         bool handlePhysics();
-
-        void handleGraphics(const bool& gameIsDone);
+        void handleGraphics();
 
         const std::shared_ptr<CanvasTransactionGenerator> _canvasTransactionGenerator;
         const std::shared_ptr<Player> _currentPlayer;
