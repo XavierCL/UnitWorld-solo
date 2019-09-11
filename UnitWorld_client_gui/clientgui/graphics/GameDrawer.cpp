ae0b99d24b7dd8a70890599525437d8dfb3d7d7d
Xavier <xavierx-men@hotmail.com>
2019-09-10 22:48:45 -0400

    Changed a bit the colors

--- UnitWorld_client_gui/clientgui/graphics/GameDrawer.cpp
+++ UnitWorld_client_gui/clientgui/graphics/GameDrawer.cpp
@@ -6,7 +6,7 @@ const std::vector<sf::Color> GameDrawer::_playerColors({
     sf::Color(0, 0, 255),
     sf::Color(255, 0, 0),
     sf::Color(0, 255, 0),
-    sf::Color(128, 255, 0),
+    sf::Color(255, 255, 0),
     sf::Color(128, 0, 128),
     sf::Color(255, 128, 0),
     sf::Color(0, 128, 255),
@@ -25,7 +25,7 @@ const std::vector<sf::Color> GameDrawer::_playerColors({
     sf::Color(0, 128, 0),
     sf::Color(0, 0, 128),
     sf::Color(128, 255, 255),
-    sf::Color(255, 255, 0),
+    sf::Color(128, 255, 0),
     sf::Color(128, 128, 128),
     sf::Color(255, 0, 255)
 });
