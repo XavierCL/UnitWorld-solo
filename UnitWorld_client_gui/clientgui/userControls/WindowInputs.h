a4d54b5055cdb003241dab3fa929b583c6f4cb26
Xavier <xavierx-men@hotmail.com>
2019-09-10 23:34:46 -0400

    updated all relevant files, just missing the code!

---
+++ UnitWorld_client_gui/clientgui/userControls/WindowInputs.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace uw
+{
+    class WindowInputs
+    {
+    public:
+        // Receives input from the window manager
+        // Sets the user control state and the camera based on those inputs
+    };
+}
