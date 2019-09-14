691ffc385b0e3012906ca975b906852dd81c9263
abstrack <lebel.louisjacob@gmail.com>
2019-09-13 20:22:36 -0400

    fill 'clientgui/userControls/WindowInputs.h'

--- UnitWorld_client_gui/clientgui/userControls/WindowInputs.h
+++ UnitWorld_client_gui/clientgui/userControls/WindowInputs.h
@@ -1,11 +1,53 @@
 #pragma once
 
+#include "UserControlState.h"
+#include "clientgui/cameras/Camera.h"
+
+#include <memory>
+
 namespace uw
 {
     class WindowInputs
     {
     public:
-        // Receives input from the window manager
-        // Sets the user control state and the camera based on those inputs
+        WindowInputs(std::shared_ptr<UserControlState> userControlState, std::shared_ptr<Camera> camera)
+        : _userControlState(userControlState),
+        _camera(camera)
+        {}
+
+        void frameHappened()
+        {
+            _userControlState->frameHappened();
+        }
+
+        void setUserMousePosition(const Vector2D& position)
+        {
+            _userControlState->setUserMousePosition(position);
+            _camera->mouseMovedTo(position);
+        }
+
+        void setUserLeftMouseUpPosition(const Vector2D& position)
+        {
+            _userControlState->setUserMousePosition(position);
+        }
+
+        void setUserLeftMouseDownPosition(const Vector2D& position)
+        {
+            _userControlState->setUserLeftMouseDownPosition(position);
+        }
+
+        void setUserRightMouseDownPosition(const Vector2D& position)
+        {
+            _userControlState->setUserRightMouseDownPosition(position);
+        }
+
+        void setUserMouseWheelScroll(const float& delta, const Vector2D& mousePosition)
+        {
+            _camera->mouseTicked(delta, mousePosition);
+        }
+
+    private:
+        const std::shared_ptr<UserControlState> _userControlState;
+        const std::shared_ptr<Camera> _camera;
     };
 }
