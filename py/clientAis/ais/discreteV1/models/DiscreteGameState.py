Fixed a few numerical inconsistencies, formula bugs and code bugs

py/clientAis/ais/discreteV1/models/DiscreteGameState.py
@@ -106,7 +106,7 @@ class DiscreteGameState:
         updatedSpawners, updatedPlayers, interactionDuration = self.executeInteractions(
             [getLastSpawnerVersion(spawnerId) for spawnerId in self.spawnersById.keys()],
             [getLastPlayerVersion(playerId) for playerId in self.playerDictionary.keys()],
-            None if restrictedDuration is None else restrictedDuration - movementDuration,
+            PhysicsEstimator.MAXIMUM_INTERACTION_DURATION if restrictedDuration is None else restrictedDuration - movementDuration,
             restrictDurationToPlayer=move.playerId
         )
         updateSpawnersAndPlayers(updatedSpawners, updatedPlayers)
@@ -242,13 +242,16 @@ class DiscreteGameState:
 
     def discreteMoveToMove(self, discreteMove: DiscreteMove) -> List[Move]:
         discretePlayer = self.playerDictionary[discreteMove.playerId]
-        singuityIds = np.array([s.id for s in self.rootGameState.singuities if s.playerId == discreteMove.playerId])
-        outOfCluster = singuityIds[~np.isin(singuityIds, discretePlayer.inCluster)]
+        ownSinguities = np.array([s for s in self.rootGameState.singuities if s.playerId == discreteMove.playerId], dtype=object)
+        singuityIds = np.array([s.id for s in ownSinguities])
+        inClusterMask = np.isin(singuityIds, discretePlayer.inCluster)
+        outOfCluster = singuityIds[~inClusterMask]
+        inClusterSpeed = np.mean([s.speed for s in ownSinguities[inClusterMask]], axis=0)
 
         moves = []
 
         if len(outOfCluster) > 0:
-            moves.append(Move.fromPosition(list(outOfCluster), discretePlayer.singuitiesMeanPosition))
+            moves.append(Move.fromPosition(list(outOfCluster), discretePlayer.singuitiesMeanPosition + inClusterSpeed * PhysicsEstimator.FULL_STOP_DURATION / 2))
 
         if discreteMove.spawnerId is not None:
             moves.append(Move.fromSpawner(discretePlayer.inCluster, arrays.first(self.rootGameState.spawners, lambda s: s.id == discreteMove.spawnerId), discreteMove.playerId))

py/clientAis/ais/discreteV1/models/DiscretePlayer.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import List, Optional, Tuple
+from typing import List, Optional
 
 import numpy as np
 from sklearn.cluster import DBSCAN
@@ -24,7 +24,7 @@ class DiscretePlayer:
         playerSinguities = [s for s in gameState.singuities if s.playerId == playerId]
 
         if len(playerSinguities) == 0:
-            return DiscretePlayer(playerId, 0, np.zeros(2), 1, 0, [])
+            return DiscretePlayer(playerId, 0, np.zeros(2), PhysicsEstimator.getMinimumStd(0), 0, [])
 
         singuityPositions = [s.position for s in playerSinguities]
 
@@ -63,13 +63,13 @@ class DiscretePlayer:
         constrainedMovementDuration = getLastingMoveDuration(originalMovementDuration)
 
         if constrainedMovementDuration == 0:
-            return self
+            return 0, self
 
         targetIsWithinStd = PhysicsEstimator.distance(self.singuitiesMeanPosition, targetPosition) <= self.singuitiesStd
 
         if targetIsWithinStd:
             ratioOfTimeSpentInside = min(constrainedMovementDuration * Singuity.MAXIMUM_SPEED_UNITS_PER_FRAME, self.singuitiesStd * 2) / (self.singuitiesStd * 2)
-            newStd = min((np.sqrt(self.singuityCount)) * ratioOfTimeSpentInside + self.singuitiesStd * (1 - ratioOfTimeSpentInside), self.singuitiesStd)
+            newStd = min(PhysicsEstimator.getMinimumStd(self.singuityCount) * ratioOfTimeSpentInside + self.singuitiesStd * (1 - ratioOfTimeSpentInside), self.singuitiesStd)
         else:
             # maximum duration on a 5000 unit map
             newStd = self.singuitiesStd * (1 + constrainedMovementDuration / 1768)
@@ -102,13 +102,13 @@ class DiscretePlayer:
 
         if PhysicsEstimator.distance(spawnerPosition, self.singuitiesMeanPosition) <= self.singuitiesStd * 2:
             newSinguitiesMean, newSinguitiesStd, newSinguitiesCount = arrays.combineMeanStdAndCount(
-                self.singuitiesMeanPosition, np.sqrt(atTargetCount), atTargetCount, unitPositionInLine, singuitiesInLineStd, unitCountInLine
+                self.singuitiesMeanPosition, np.sqrt(atTargetCount), atTargetCount, unitPositionInLine, singuitiesInLineStd, unitCountInLine, PhysicsEstimator.getMinimumStd(atTargetCount + unitCountInLine)
             )
         else:
             newSinguitiesMean, newSinguitiesStd, newSinguitiesCount = self.singuitiesMeanPosition, np.sqrt(atTargetCount), atTargetCount
 
         singuitiesMean, singuitiesStd, singuitiesCount = arrays.combineMeanStdAndCount(
-            self.singuitiesMeanPosition, np.array([self.singuitiesStd]), self.singuityCount, newSinguitiesMean, np.linalg.norm(newSinguitiesStd), newSinguitiesCount
+            self.singuitiesMeanPosition, np.array([self.singuitiesStd]), self.singuityCount, newSinguitiesMean, np.linalg.norm(newSinguitiesStd), newSinguitiesCount, PhysicsEstimator.getMinimumStd(newSinguitiesCount + self.singuityCount)
         )
 
         return DiscretePlayer(self.id, singuitiesCount, singuitiesMean, np.linalg.norm(singuitiesStd), 0 if self.singuityCount + newSinguitiesCount == 0 else (self.singuitiesAverageHealth * self.singuityCount + Singuity.MAX_HEALTH_POINT * newSinguitiesCount) / (self.singuityCount + newSinguitiesCount))

py/clientAis/ais/discreteV1/physics/PhysicsEstimator.py
@@ -1,3 +1,4 @@
+import math
 from typing import List, Optional, Tuple, Union
 
 import numpy as np
@@ -6,8 +7,15 @@ from clientAis.games.GameState import Singuity, Spawner
 from utils import arrays
 
 class PhysicsEstimator:
+    FULL_STOP_DURATION = Singuity.MAXIMUM_SPEED_UNITS_PER_FRAME / Singuity.MAXIMUM_ACCELERATION_PER_FRAME
     SINGUITY_ATTACK_PER_FRAME = Singuity.ATTACK_STRENGTH / Singuity.ATTACK_FRAME_LAG
     SPAWNER_SPAWN_PER_FRAME = 1 / Spawner.SPAWN_FRAME_LAG
+    MAXIMUM_SINGUITY_DENSITY_PER_AREA = 100 / (math.pi * 18 ** 2)
+    MAXIMUM_INTERACTION_DURATION = 2 * 5000 / Singuity.MAXIMUM_SPEED_UNITS_PER_FRAME
+
+    @staticmethod
+    def getMinimumStd(singuityCount: int) -> float:
+        return max(1., math.sqrt(singuityCount / (math.pi * PhysicsEstimator.MAXIMUM_SINGUITY_DENSITY_PER_AREA)))
 
     @staticmethod
     def distanceToSpawningSinguities(distance: float) -> int:
@@ -105,11 +113,13 @@ class PhysicsEstimator:
 
     @staticmethod
     def getClusterForce(singuityCount: int, clusterStd: float, averageHealth: float) -> float:
-        return ((singuityCount * averageHealth) / clusterStd) ** 2
+        return singuityCount ** 2 * averageHealth / (math.pi * clusterStd ** 2 * PhysicsEstimator.MAXIMUM_SINGUITY_DENSITY_PER_AREA)
 
     @staticmethod
     def clusterForceToCount(clusterForce: float, clusterStd: float, averageHealth: float) -> int:
-        return np.sqrt(clusterForce) * clusterStd / averageHealth
+        if averageHealth == 0:
+            return 0
+        return round(math.sqrt(clusterForce * math.pi * clusterStd ** 2 * PhysicsEstimator.MAXIMUM_SINGUITY_DENSITY_PER_AREA / averageHealth))
 
     # Takes in a list of (singuityCount, clusterStd, singuityAverageHealth) and returns a list of singuity count
     @staticmethod
@@ -118,13 +128,16 @@ class PhysicsEstimator:
         clusterForces = [PhysicsEstimator.getClusterForce(*cluster) for cluster in clusters]
         maximalForceClusterIndex = np.argmax(clusterForces)
         averageAdversaryForce = (np.sum(clusterForces) - clusterForces[maximalForceClusterIndex]) / (len(clusters) - 1)
-        maximalRemainingForce = maximalForceClusterIndex - maximalForceClusterIndex / min(
+
+        maximalRemainingForce = (clusterForces[maximalForceClusterIndex] - clusterForces[maximalForceClusterIndex] / min(
             Singuity.MAX_HEALTH_POINT / Singuity.ATTACK_STRENGTH, clusterForces[maximalForceClusterIndex] / averageAdversaryForce
-        ) ** 2
+        ) ** 2) if averageAdversaryForce > 1 else clusterForces[maximalForceClusterIndex]
+
         return arrays.assign(
-            np.zeros(len(clusters)), maximalForceClusterIndex,
+            np.zeros(len(clusters)),
+            maximalForceClusterIndex,
             PhysicsEstimator.clusterForceToCount(maximalRemainingForce, clusters[maximalForceClusterIndex][1], clusters[maximalForceClusterIndex][2])
-            )
+        )
 
     # Takes in clusters as a list of (singuityCount, clusterStd, singuityAverageHealth) and returns a tuple of (spawnerRemainingHealth, interactionDuration, [singuityCount])
     @staticmethod
@@ -146,9 +159,9 @@ class PhysicsEstimator:
             remainingCounts = np.array([clusters[0][1]])
         else:
             averageAdversaryForce = (np.sum(clusterForces) - clusterForces[maximalForceClusterIndex]) / (len(clusters) - 1)
-            maximalRemainingForce = clusterForces[maximalForceClusterIndex] - clusterForces[maximalForceClusterIndex] / min(
+            maximalRemainingForce = (clusterForces[maximalForceClusterIndex] - clusterForces[maximalForceClusterIndex] / min(
                 Singuity.MAX_HEALTH_POINT / Singuity.ATTACK_STRENGTH, clusterForces[maximalForceClusterIndex] / averageAdversaryForce
-                ) ** 2
+                ) ** 2) if averageAdversaryForce > 1 else clusterForces[maximalForceClusterIndex]
 
             if homeClusterIndex == maximalForceClusterIndex:
                 maximalRemainingForce /= spawnerRemainingHealth / Spawner.MAX_HEALTH_POINTS + 1

py/utils/arrays.py
@@ -40,19 +40,21 @@ def first(arr: Iterable[Nested], predicate: Callable[[Nested], bool]) -> Nested:
 
     raise Exception("Could not find first value within array")
 
-def combineMeanStdAndCount(mean1: Nested = 0, std1: Nested = 1, count1: int = 1, mean2: Nested = 0, std2: Nested = 1, count2: int = 1) -> Tuple[Nested, Nested, int]:
+def combineMeanStdAndCount(mean1: Nested = 0, std1: Nested = 1, count1: int = 1, mean2: Nested = 0, std2: Nested = 1, count2: int = 1, minStd: Optional = None) -> Tuple[Nested, Nested, int]:
     if count1 < 1 and count2 < 1:
-        return (mean1 + mean2) / 2, 0, 0
+        return (mean1 + mean2) / 2, 1 if minStd is None else minStd, 0
     elif count1 < 1:
         return mean2, std2, count2
     elif count2 < 1:
         return mean1, std1, count1
 
+    intermediateStd = np.sqrt(
+        ((count1 - 1) * std1 ** 2 + (count2 - 1) * std2 ** 2) / (count1 + count2 - 1) + (count1 * count2 * (mean1 - mean2) ** 2) / ((count1 + count2) * (count1 + count2 - 1))
+    )
+
     return (
         (mean1 * count1 + mean2 * count2) / (count1 + count2),
-        np.sqrt(
-            ((count1 - 1) * std1 ** 2 + (count2 - 1) * std2 ** 2) / (count1 + count2 - 1) + (count1 * count2 * (mean1 - mean2) ** 2) / ((count1 + count2) * (count1 + count2 - 1))
-        ),
+        np.max([intermediateStd, np.zeros_like(intermediateStd) if minStd is None else np.ones_like(intermediateStd) * minStd], axis=0),
         count1 + count2
     )
 